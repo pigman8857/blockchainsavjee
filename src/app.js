2b6fada6b6e0df43c4ac45650a38d01077643d43
OhSva <somkamolv@hotmail.com>
2020-01-11 20:55:21 +0700

    Proof of work tutorial

--- src/app.js
+++ src/app.js
@@ -1,11 +1,76 @@
+const SHA256 = require("crypto-js/sha256");
 class Block {
   constructor(index, timestamp, data, previousHash = "") {
     this.index = index;
+    this.previousHash = previousHash;
     this.timestamp = timestamp;
     this.data = data;
-    this.previousHash = previousHash;
-    this.hash = "";
+    this.hash = this.calculateHash();
+    this.nonce = 0;
   }
 
-  calculateHash() {}
+  calculateHash() {
+    return SHA256(
+      `${this.index}${this.previousHash}${this.timestamp$}${JSON.stringify(
+        this.data
+      )}${this.nonce}`
+    ).toString();
+  }
+
+  mineBlock(difficulty) {
+    while (
+      this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")
+    ) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+
+    console.log(`Block Mined : ${this.hash}`);
+  }
 }
+
+class BlockChain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.difficulty = 2;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2020", "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLatestBlock().hash;
+    newBlock.mineBlock(this.difficulty);
+
+    this.chain.push(newBlock);
+  }
+
+  isChainValid() {
+    for (let i = 1; i < this.chain.length; i++) {
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+
+      if (currentBlock.hash !== currentBlock.calculateHash()) {
+        return false;
+      }
+
+      if (currentBlock.previousHash !== previousBlock.hash) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+}
+
+let savjeeCoin = new BlockChain();
+console.log(savjeeCoin);
+console.log("mine block 1..");
+savjeeCoin.addBlock(new Block(1, "1/11/2020", { amount: 4 }));
+console.log("mine block 2..");
+savjeeCoin.addBlock(new Block(2, "1/12/2020", { amount: 10 }));
