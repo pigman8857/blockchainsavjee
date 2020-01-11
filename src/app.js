0119ea4d74da543ddf2f2c872724f827150756ec
OhSva <somkamolv@hotmail.com>
2020-01-11 16:51:06 +0700

    Install --save crypto-js

---
+++ src/app.js
@@ -0,0 +1,11 @@
+class Block {
+  constructor(index, timestamp, data, previousHash = "") {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.data = data;
+    this.previousHash = previousHash;
+    this.hash = "";
+  }
+
+  calculateHash() {}
+}
